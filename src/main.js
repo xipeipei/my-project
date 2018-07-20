5369373cc41a7991404062e289de41ce9da33d16
xipeipei <xipeipei@thunisoft.com>
2018-07-20 19:21:23 +0800

    就先这些..

--- src/main.js
+++ src/main.js
@@ -3,13 +3,25 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/styles/index.scss' // global css
+import 'normalize.css/normalize.css'
+import 'element-ui/lib/theme-chalk/index.css'
+import i18n from './lang'
+import './icons'
 
+Vue.use(ElementUI, {
+  VueI18n: (key, value) => i18n.t(key, value)
+})
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  store,
+  i18n,
+  render: h => h(App)
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+const _import = require('./_import_' + process.env.NODE_ENV)
+import Layout from '../views/layout/Layout'
 
 Vue.use(Router)
 
+export const constantRouterMap = [
+  { path: '/login', component: _import('login/index'), hidden: true },
+  { path: '/upload', component: _import('login/upload'), hidden: true },
+  { path: '/authredirect', component: _import('login/authredirect'), hidden: true }
+]
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap
 })
+
+export const asyncRouterMap = [
+  {
+    path: '/i18n',
+    component: Layout,
+    children: [{ path: 'index', component: _import('i18n-demo/index'), name: 'i18n', meta: { title: 'i18n', icon: 'international' }}]
+  },
+
+  { path: '*', redirect: '/404', hidden: true }
+]
+
